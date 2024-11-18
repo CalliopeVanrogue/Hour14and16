e5c420a588e739ff2325531141d03c125f547e18
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-11-17 23:51:13 -0800

    Hour 14 and 16 complete

---
+++ Cpilkington_Hour14and16/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+    
+
+// Size of the particle effect
+    // shape of the particle effect (circle instead of cone)
+    // Rate of particles
+    // color of particles including a fade as they fall
+    //randomized directions of particles
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
