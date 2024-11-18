2ac3e9d6d45fea265e207f5b2ec18a2ae0e4bda5
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-11-17 22:09:57 -0800

    Menu screen complete

---
+++ Cpilkington_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    // Background Image changed
+    // colors modified
+    // font styles and typeface changed
+    // button colors and wording were modified as well
+    }
